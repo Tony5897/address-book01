cdbf92f861529ee7b136c03877492739c84015e4
tonym <tony.martinez5897@gmail.com>
2022-01-25 09:30:37 -0800

    Initial Commit
    Co-authored-by: Matt Eilar <meilar@gmail.com>
    Co-authored-by: Ryan Spencer <ryan.spencer1220@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
+// Business Logic fo AddressBook --------
 function AddressBook() {
   this.contacts = {};
+  this.currentId = 0;
 }
 
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts[contact.firstName] = contact;
+  return this.currentId;
+};
+
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+
+// Business Logic for Contacts -----------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
