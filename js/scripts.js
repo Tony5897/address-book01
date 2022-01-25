b1639cec3303a3fca0092b7f1b36ccbce2957835
tonym <tony.martinez5897@gmail.com>
2022-01-25 10:56:51 -0800

    Add functionality to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -42,9 +42,26 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
 
+let contact = new Contact("Ada", "Lovelace", "111-111-1111");
+
+for (const key in contact) {
+  if (contact.hasOwnProperty(key)) {
+  console.log(contact[key]);
+  }
+}
 
 // Interface Logic ---------
 let addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    htmlForContactInfo += "<li id=" + contouchstart.id + ">" + contact.firstName + " " + contact.lastName +"</li>";
+  });
+  contacts.html(htmlForContactInfo);
+}
 
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
@@ -52,7 +69,6 @@ $(document).ready(function() {
     const inputtedFirstName = $("input#new-first-name").val();
     const inputtedLastName = $("input#new-last-name").val();
     const inputtedPhoneNumber = $("input#new-phone-number").val();
-
     let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
     addressBook.addContact(newContact);
     console.log(addressBook.contacts);
